4dc423a3e3da950d9b6ea87c58045fa159cc74db
AleksandrTurlakov <aturlakov@ukr.net>
2023-02-13 07:32:56 +0200

    create CLI App

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # NodeJS-HW-01
+
+# Получаем и выводим весь список контактов в виде таблицы
+
+node index.js --action list
+https://monosnap.com/file/umSZPu8tv4pKoB8vXzamJAjwY2vE6E
+
+# Получаем контакт по id
+
+node index.js --action get --id 5
+https://monosnap.com/file/kma4iXxNV0qWhOM41hBnkDN7rtRZcU
+
+# Добавялем контакт
+
+node index.js --action add --name Mango --email mango@gmail.com --phone 322-22-22
+https://monosnap.com/file/19H7AeYa8ZKKRlxWUwKyzanpBaK5GY
+
+# Удаляем контакт
+
+node index.js --action remove --id=3
+https://monosnap.com/file/MH5yonBAL2mbwCMt5M3KCWj4OmRYpN
---
+++ contacts.js
@@ -0,0 +1,29 @@
+const contactsOperatons = require("./db");
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const contacts = await contactsOperatons.listContacts();
+      console.table(contacts);
+      break;
+    case "get":
+      const contact = await contactsOperatons.getContactById(id);
+      if (!contact) {
+        throw new Error(`Contact with id=${id} not found`);
+      }
+      console.log(contact);
+      break;
+    case "add":
+      const newContact = await contactsOperatons.addContact(name, email, phone);
+      console.log(newContact);
+      break;
+    case "remove":
+      const removeContact = await contactsOperatons.removeContact(id);
+      console.log(removeContact);
+      break;
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
+
+module.exports = invokeAction;
